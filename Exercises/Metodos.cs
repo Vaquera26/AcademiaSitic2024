dbc9524b21038a1845887ad05c505d9416520118
Juan Vaquera <juan.tec@outlook.com>
2024-09-28 10:39:11 -0600

    Acutalizacion, se aprendio sobre los metodos

---
+++ Exercises/Metodos.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises
+{
+    internal class Metodos
+    {
+        // Metodos
+        static void Main2(string[] args)
+        {
+
+            int initialStock = 50;
+            int quantityToAdd = 20;
+            int addedQuantity;
+
+
+
+
+            UpdateStock(initialStock, quantityToAdd, out int updatedStock, out addedQuantity);
+
+            Console.WriteLine($"Inventario Inicial: {initialStock}");
+            Console.WriteLine($"Cantidad Agregada: {quantityToAdd}");
+            Console.WriteLine($"Inventario Actualizado: {updatedStock}");
+
+            // Ajuste de entrada
+            AdjustStock(ref updatedStock, 10);
+            Console.WriteLine($"Ajuste de entrada: {updatedStock}");
+
+            //Ajuste de Salida
+            AdjustStock(ref updatedStock, -20);
+            Console.WriteLine($"Ajuste de salida: {updatedStock}");
+
+            //Lectura de producto
+            // El var ocupa cierto espacio de memoria por que no conoce el tipo de dato
+            // var productInfo = GetProductInfo("Laptop", 20);
+            // productInfo.productName
+            
+           (string productName, int stock) = GetProductInfo("Laptop", 20);
+            Console.WriteLine($"Nombre del product: {productName}");
+            Console.WriteLine($"Inventario del producto: {stock}");
+            
+            Console.ReadKey();
+        }
+
+        public static void UpdateStock(int initialSotck, int quantityToAdd, out int updatedStock, out int addedQuantity)
+        {
+
+            addedQuantity = quantityToAdd;
+            updatedStock = initialSotck + addedQuantity;
+        }
+
+        public static void AdjustStock(ref int stock, int adjustment)
+        {
+            stock += adjustment;
+        }
+
+        public static (string productName, int stock) GetProductInfo(string productName, int stock)
+        {
+            return (productName, stock);
+        }
+
+
+    }
+}
--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -5,108 +5,33 @@ namespace Exercises
 {
     internal class Program
     {
+
+        /*
+        // Metodos
         static void Main(string[] args)
         {
 
-            string  courseName      = "Academia SITIC" ;
+            int a = 5;
+            int b = 10;  
 
-            String  courseName2     = "Academia SITIC 2" ;
-
-            int     studentsCount   = 28 ;
-
-            bool    isStartingNow   = true ;
-           
-            int?   age              = null ;
-
-            /*
-             
-            // 1
-            Console.WriteLine (    age != null ? age : 0    );
-
-            // 2
-            if (age != null)
-            {
-                Console.WriteLine(age);
-            }
-            else
-            {
-                Console.WriteLine(0);
-            }
-
-            // 3
-            Console.WriteLine ( age.GetValueOrDefault ( 0 ) );
-
+            Console.WriteLine(SumInteger(out a, b));
+            Console.WriteLine(a);
+            Console.WriteLine(b);
             Console.ReadKey();
 
-            */
+        }   
+        
+        // Valor por Referencia (ref), haga lo que haga 
+        // Valor out, no importa si lo inicializas, en el ref tiene que estar instanciado atnes de qut lo envies
 
-            User user = new();
-            Employer emp = new();
-
-            
-
-     
-        }
-
-        public class User
-        {
-            private int _idUser;
-            private string _name;
-            private string _password;
-
-            public string Password { get => _password; set => _password = value; }
-
-            
-            /*
-            //Short Size
-            public int IdUser { get; set; }
-
-            // Medium Size
-            private string _password;
-
-            public string Password
-            {
-                get
-                {
-                    return _password;
-                }
-
-                set
-                {
-                    _password = value;  
-                }
-
-            }
-
-            // Large Size
-            public string GetName()
-            {
-                return _name;
-            }
-
-            public void SetName(string name )
-            {
-                _name = name;
-            }
-
-            */
-
-        }
-
-        public class Person
-        {
-            public int PersonId { get; set; }
-            public string Name { get; set; }
-
-        }
-
-        public class Employer : Person
+        public static int SumInteger(out int a, int b) 
         {
 
-            public DateTime StarDate { get; set; }
+            a = b;
+            return a + b;
 
         }
 
-
+        */
     }
 }
